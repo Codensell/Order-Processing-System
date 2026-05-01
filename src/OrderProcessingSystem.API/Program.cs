08c66c244678b7f5dea76c0218cfe44c622bef15
Codensell <sadikov91@gmail.com>
2026-05-01 16:24:52 +0500

    Add EF Core infrastructure and configure DbContext
    Installed EF Core and PostgreSQL provider. Implemented AppDbContext and configured mapping for Order aggregate and OrderItem entity, including private field access for items collection.

--- src/OrderProcessingSystem.API/Program.cs
+++ src/OrderProcessingSystem.API/Program.cs
@@ -1,10 +1,14 @@
 using OrderProcessingSystem.Application.Orders.CreateOrder;
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<ICreateOrderService, CreateOrderService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql("Host=localhost;Port=5432;Database=orders_db;Username=postgres;Password=postgres"));
 
 var app = builder.Build();
 
---
+++ src/OrderProcessingSystem.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using OrderProcessingSystem.Domain.Entities;
+
+namespace OrderProcessingSystem.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Order> Orders => Set<Order>();
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(builder =>
+        {
+           builder.HasKey(o => o.Id);
+
+           builder.Property(o => o.CreatedAt).IsRequired();
+
+           builder.Property(o => o.Status).HasConversion<int>().IsRequired();
+
+           builder.HasMany(typeof(OrderItem), "_items").WithOne().HasForeignKey("OrderId");
+        });
+
+        modelBuilder.Entity<OrderItem>(builder =>
+        {
+           builder.HasKey(i => i.Id);
+
+           builder.Property(i => i.ProductName).IsRequired();
+           builder.Property(i => i.Price).IsRequired();
+           builder.Property(i => i.Quantity).IsRequired();
+        });
+    }
+}
